36f9fc0e33929cf1746f658e0d7221d35cd795db
Rowan Evenstar <zenrhe@gmail.com>
2018-02-16 20:54:57 +0000

    Updated Page Title and added DS Store to .ignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Arwen Studios</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,7 +79,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   <h1> Arwen Studios   </h1>
+                   <h2> Home of Rowan Evenstar</h2>
+
                 </div>
 
                 <div class="links">
